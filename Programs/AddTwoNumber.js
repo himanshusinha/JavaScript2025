4300bfd0eea8cfc2e95782e597c310457420d0e0
hsinha1110 <97324633+hsinha1110@users.noreply.github.com>
2025-11-21 22:33:25 +0530

    equality operator

---
+++ Programs/AddTwoNumber.js
@@ -0,0 +1,5 @@
+// Add two numbers and print the result
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+console.log(`Sum of ${num1} and ${num2} is: ${sum}`);
---
+++ Programs/SquareRoot.js
@@ -0,0 +1,4 @@
+// Square root of a number
+let number = 16;
+let sqaureRoot = Math.sqrt(number);
+console.log(sqaureRoot);
