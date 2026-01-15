39e146d021e1e7b43a4771213fe0a5ce1777239c
hsinha1110 <himanshusinha1110@gmail.com>
2026-01-15 21:23:31 +0530

    Javascript Programs

--- Udemy/Jonas/01-JavaScript-Fundamentals-Part-01/script.js
+++ Udemy/Jonas/01-JavaScript-Fundamentals-Part-01/script.js
@@ -4,3 +4,25 @@
 // if (js === "amazing") alert("JavaScript is FUN !!!!");
 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+// Values
+console.log("Jonas");
+console.log(23);
+
+// Variables
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+let person = "Jonas";
+let PI = 3.1415;
