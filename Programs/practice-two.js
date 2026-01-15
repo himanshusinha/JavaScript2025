390a0a4f24d07282185d71aa70f3ea8e0a88a89d
hsinha1110 <himanshusinha1110@gmail.com>
2026-01-15 21:23:09 +0530

    Javascript Programs

--- Programs/practice-two.js
+++
@@ -1,8 +0,0 @@
-const num = [1, 2, [3, 4], [5, 6]];
-const x = num.toString();
-const y = x.split(",");
-let finalArray = [];
-y.map((item) => {
-  finalArray.push(parseInt(item));
-});
-console.log(finalArray);
--- Programs/practice.js
+++ Programs/practice.js
@@ -1,11 +1,18 @@
+// 1 JavaScript Program To Print Hello World
 console.log("Hello World");
 
-// add two numbers
-const a = 10,
-  b = 20,
-  c = a + b;
-console.log("Sum of two numbers a and b is :", c);
+// 2 JavaScript Program to Add Two Numbers
+const a = 20;
+b = 10;
+c = a + b;
+console.log("Addition of a and b is : " + c);
 
-//find square root of number
+// 3 JavaScript Program to Find the Square Root
 const num = 25;
-console.log(Math.sqrt(num));
+console.log("Square root of num is : " + Math.sqrt(num));
+
+// 4 JavaScript Program to Calculate the Area of a Triangle
+const d = 20;
+const e = 30;
+const f = (1 / 2) * d * e;
+console.log("Area of triangle is : " + f);
