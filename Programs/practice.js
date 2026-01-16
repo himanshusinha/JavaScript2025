81768bf7c52090da244635288adae53bba4daeca
hsinha1110 <himanshusinha1110@gmail.com>
2026-01-16 12:59:44 +0530

    16.1.2026 4 Programs

--- Programs/practice.js
+++ Programs/practice.js
@@ -1,18 +1,62 @@
-// 1 JavaScript Program To Print Hello World
-console.log("Hello World");
+/* Date 15.1.2026 */
 
-// 2 JavaScript Program to Add Two Numbers
-const a = 20;
-b = 10;
-c = a + b;
-console.log("Addition of a and b is : " + c);
+// // 1 JavaScript Program To Print Hello World
+// console.log("Hello World");
 
-// 3 JavaScript Program to Find the Square Root
-const num = 25;
-console.log("Square root of num is : " + Math.sqrt(num));
+// // 2 JavaScript Program to Add Two Numbers
+// const a = 20;
+// b = 10;
+// c = a + b;
+// console.log("Addition of a and b is : " + c);
 
-// 4 JavaScript Program to Calculate the Area of a Triangle
-const d = 20;
-const e = 30;
-const f = (1 / 2) * d * e;
-console.log("Area of triangle is : " + f);
+// // 3 JavaScript Program to Find the Square Root
+// const num = 25;
+// console.log("Square root of num is : " + Math.sqrt(num));
+
+// // 4 JavaScript Program to Calculate the Area of a Triangle
+// const d = 20;
+// const e = 30;
+// const f = (1 / 2) * d * e;
+// console.log("Area of triangle is : " + f);
+
+/* Date 16.1.2026 */
+
+// 5 JavaScript Program to Swap Three Variables
+
+let a = 10;
+let b = 20;
+let temp;
+
+temp = a;
+a = b;
+b = temp;
+
+console.log("Value of a after swapping :", a);
+console.log("Value of b after swapping :", b);
+
+// 6 Javascript Program to Generate a Random Number 1 to 6
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+// 7 Javascript Program to Check if a number is Positive, Negative, or Zero
+
+const num = "asdsad";
+
+if (num > 0) {
+  console.log("Number is positive");
+} else if (num < 0) {
+  console.log("Number is negative");
+} else if (num === 0) {
+  console.log("Number is zero");
+} else {
+  console.log("Number is nor positive , negative and zero");
+}
+
+// 8 Javascript Program to Check if a Number is Odd or Even
+
+const number = 3;
+
+if (number % 2 === 0) {
+  console.log("Number is even");
+} else {
+  console.log("Number is odd");
+}
