dd24a7e393a6691904d7bc38196adc7505665609
hsinha1110 <himanshusinha1110@gmail.com>
2026-02-11 22:13:07 +0530

    date 11.2.2026 5 programs added

--- Programs/practice_questions.js
+++ Programs/practice_questions.js
@@ -32,3 +32,46 @@ if (x > y) {
 for (let i = 1; i <= 5; i++) {
   console.log(i);
 }
+
+// 11.2.2026
+
+// 1 Square root of number
+
+const number = 25;
+console.log(Math.sqrt(number));
+
+// 2 Swap two number
+let num1 = 10;
+let num2 = 20;
+let temp;
+
+// temp = num1;
+// num1 = num2;
+// num2 = temp;
+
+[num1, num2] = [num2, num1];
+console.log(`The value of num1 after swapping: ${num1}`);
+console.log(`The value of num2 after swapping: ${num2}`);
+
+// 3 Generate random number
+console.log(Math.trunc(Math.random() * 5) + 1);
+
+// 4 Check the number is positive negative or zero
+let numb = 5;
+
+if (numb > 0) {
+  console.log("Number is positive");
+} else if (numb < 0) {
+  console.log("Number is negative");
+} else if (numb === 0) {
+  console.log("Number is zero");
+} else {
+  console.log("Number is not valid ");
+}
+
+//5 Area of triangle
+
+const aa = 10;
+const bb = 20;
+
+console.log((1 / 2) * aa * bb);
