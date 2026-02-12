ef6878f1874f190fefe652f5df6971f3596fe606
hsinha1110 <himanshusinha1110@gmail.com>
2026-02-12 21:10:05 +0530

    program

--- Udemy/Jonas/09-Data-Structures-Operators/script.js
+++ Udemy/Jonas/09-Data-Structures-Operators/script.js
@@ -1,4 +1,27 @@
+// Data needed for first part of the section
 "use strict";
+
+// Data needed for a later exercise
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+const italianFoods = new Set([
+  "pasta",
+  "gnocchi",
+  "tomatoes",
+  "olive oil",
+  "garlic",
+  "basil",
+]);
+
+const mexicanFoods = new Set([
+  "tortillas",
+  "beans",
+  "rice",
+  "tomatoes",
+  "avocado",
+  "garlic",
+]);
 
 // Data needed for first part of the section
 const restaurant = {
@@ -7,8 +30,20 @@ const restaurant = {
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
   },
 };
 
@@ -33,4 +68,33 @@ let [main, , secondary] = restaurant.categories;
 [main, secondary] = [secondary, main];
 console.log(main, secondary);
 
-console.log(restaurant.order(2, 0));
+// console.log(restaurant.order(2, 0));
+
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// Nested Destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, j] = nested;
+const [i, , [j, k]] = nested;
+console.log(1, j, k);
+
+// Default value
+const [p = 1, q = 1, r = 1] = [8];
+console.log(p, q, r);
+
+//Destructuring Objects
+
+const { name, openingHours, categories } = restaurant;
+console.log(restaurant);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+
+console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
