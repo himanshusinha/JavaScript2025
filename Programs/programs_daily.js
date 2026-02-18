da84e492546985f19e957c4339fe5225359273a5
hsinha1110 <himanshusinha1110@gmail.com>
2026-02-18 12:46:14 +0530

    5 programs 18.2.2026

--- Programs/programs_daily.js
+++ Programs/programs_daily.js
@@ -1,5 +1,5 @@
 /* Date 17.2.2026 */
-
+/*  
 const num = [5, 15, 22, 30, 10, 18];
 
 // 10 se bade numbers
@@ -36,3 +36,57 @@ for (let i = 0; i < num.length; i++) {
   }
 }
 console.log(sum);
+*/
+
+/* Date 18.2.2026 */
+/*  
+// 3 se divisible numbers or count bhi print karo
+let count = 0;
+const arr = [5, 15, 22, 30, 10, 18];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 3 === 0) {
+    console.log("Number divisible by 3 is", arr[i]);
+    count++;
+  }
+}
+console.log("Count is", count);
+
+// 5 se divisible numbers or count bhi print karo
+let count = 0;
+const arr = [5, 15, 22, 30, 10, 18];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 5 === 0) {
+    console.log("Number divisible by 5 is", arr[i]);
+    count++;
+  }
+}
+console.log("Count is", count);
+
+// 2 or 3 dono se divisible number print karo aur count bhi
+const arr = [5, 15, 22, 30, 10, 18];
+let count = 0;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 === 0 && arr[i] % 3 === 0) {
+    console.log("Numer divisble by 2 and 3 is", arr[i]);
+    count++;
+  }
+}
+console.log(count);
+
+// Range check 5-10 tak ke numbers print karo
+const numbers = [2, 3, 4, 6, 12, 15, 18, 20];
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 5 && numbers[i] < 10) {
+    console.log(numbers[i]);
+  }
+}
+
+// Range 50-100 tak ke numbers print karo
+const numbers = [55, 60, 75, 80.95, 100, 120];
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 50 && numbers[i] < 100) {
+    console.log(numbers[i]);
+  }
+}
+*/
