7f19fb4501a0991440002225c430144f85bf9615
hsinha1110 <himanshusinha1110@gmail.com>
2026-03-28 16:51:01 +0530

    daily 5 programs interview 5 programs

---
+++ W3Schools/Programs/daily.js
@@ -0,0 +1,49 @@
+// Date 28.3.2026
+
+// 1 JavaScript Program to Add Two Numbers
+
+const x = 10;
+const y = 20;
+const z = x + y;
+
+console.log("Addition of x and y :", z);
+
+// 2 JavaScript Program for Multiplication of Two Numbers
+
+const a = 10;
+const b = 20;
+const c = a * b;
+
+console.log("Multiplication of a and b :", c);
+
+// 3 JavaScript Program to Add Float Numbers
+
+const h = 10.3;
+const p = 20.5;
+const d = h + p;
+
+console.log("Addition of h and p :", d);
+
+// 4 JavaScript Program to Check if a Number is Odd or Even
+
+const num = 5;
+
+if (num % 2 === 0) {
+  console.log("Number is Even");
+} else {
+  console.log("Number is Odd");
+}
+
+// 5 JavaScript Program to Check if a Number is Positive, Negative, or Zero
+
+const checkNum = 5;
+
+if (checkNum > 0) {
+  console.log("Number is Positive");
+} else if (checkNum < 0) {
+  console.log("Number is Negative");
+} else if (checkNum === 0) {
+  console.log("Number is Zero");
+} else {
+  console.log("Invalid Number");
+}
